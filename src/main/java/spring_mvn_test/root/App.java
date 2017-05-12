923a86eee8f4d9a20e292350322340a0a5cfdcac
mugner <goorka1@hotmail.com>
2017-05-12 13:55:57 -0700

    First commit

---
+++ src/main/java/spring_mvn_test/root/App.java
@@ -0,0 +1,13 @@
+package spring_mvn_test.root;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
